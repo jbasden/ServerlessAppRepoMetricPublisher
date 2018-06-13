f722cfdfafd2bdd5d53dee359272928b87abaeb1
Joshua Basden <jcbasden@amazon.com>
2018-06-13 10:24:04 -0700

    Add schema files for request validation.
    This commit adds the JSON schema for log_events and metric_datum.
    It also adds the core logic for schema.py.

--- metricpublisher/lambda_handler.py
+++ metricpublisher/lambda_handler.py
@@ -1,1 +1,14 @@
-"""skeleton docstring."""
+"""Lambda function entrypoint handlers."""
+
+
+def log_event(event, context):
+    """Log event.
+
+    Parameters:
+        event (dict): the user input
+
+    Returns:
+        (str) message upon successful execution
+
+    """
+    return "Function executed through to completion."
